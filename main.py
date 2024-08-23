aa244c88a7aae3f9b55ad4aaf02cf7e9de37a892
Snowy <snowycold1068@gmail.com>
2024-08-23 18:24:38 +0200

    program starter

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from downloader import Downloader
 from gui import YouTubeDownloaderGUI
 from queue_manager import QueueManager
 
-def start_download_callback(url, resolution):
-    queue_manager.start_download(url, resolution)
+def start_download_callback(url, resolution, download_subtitles, output_format, subtitle_language=None):
+    queue_manager.start_download(url, resolution, download_subtitles, output_format, subtitle_language)
 
 def stop_download_callback():
     queue_manager.stop_download()
+    gui.stop_button.pack_forget()
 
 if __name__ == "__main__":
     root = tk.Tk()
